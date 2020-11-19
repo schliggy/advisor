cb3644e4f07d4fee89db57ae27edd0eb7f386b4d
Marius Hadry <marius.hadry@stud-mail.uni-wuerzburg.de>
2020-11-19 17:57:19 +0100

    set random seed to a fixed number

--- advisor_server/manage.py
+++ advisor_server/manage.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
+import random
+
 
 if __name__ == "__main__":
+  random.seed(1)
   os.environ.setdefault("DJANGO_SETTINGS_MODULE", "advisor.settings")
   try:
     from django.core.management import execute_from_command_line
